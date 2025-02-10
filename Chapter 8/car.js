bb5f0f16fa14eca2fadfccb4e8f77b745e00afae
Courts02 <courtneycook2006@gmail.com>
2025-02-10 16:58:44 +0200

    Chapters 8-10

---
+++ Chapter 8/car.js
@@ -0,0 +1,28 @@
+// var object = {
+//     "key1": 3,
+//     "key2": true
+// };
+
+// console.log(object["key1"])
+
+// var object = {
+//     "1": "Mom",
+//     "2": "Daniel",
+//     "3": "Tristen",
+// }
+
+// console.log(typeof object["3"])
+
+var dreamCar = {
+    "Make" : "Porsche ",
+    "Model" : " 03",
+    "Color" : "green",
+    "Year" : 2003,
+    "BodyStyle" : "Luxury Car",
+    "Price" : 4500
+};
+
+document.getElementById("modelyear").innerHTML = dreamCar.Year;
+document.getElementById("pricetag").innerHTML = dreamCar.Price
+document.getElementById("body").innerHTML = dreamCar.Make + " GT3 RS" + dreamCar.Model
+document.getElementById("body").style.backgroundColor = dreamCar.Color
